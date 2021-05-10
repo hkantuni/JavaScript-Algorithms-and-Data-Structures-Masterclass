97f851c6c95117f6118680e768db4fa290d5fdb3
Hovhannes Kantuni <hkantuni@gmail.com>
2021-05-10 17:38:26 -0400

    Recursion Coding Exercise 20: nestedEvenSum

--- nestedEvenSum.js
+++ nestedEvenSum.js
@@ -31,8 +31,9 @@ var obj2 = {
   a: 2,
   b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
   c: { c: { c: 2 }, cc: "ball", ccc: 5 },
-  d: 1,
+  d: 2,
   e: { e: { e: 2 }, ee: "car" },
 };
+
 console.log(nestedEvenSum(obj1)); // 6
-console.log(nestedEvenSum(obj2)); // 10
+console.log(nestedEvenSum(obj2)); // 12
