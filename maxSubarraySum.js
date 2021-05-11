6e571e5a2804359ec4288816ccb1c2d8679b7d59
Hovhannes Kantuni <hkantuni@gmail.com>
2021-05-11 19:18:07 -0400

    Sliding Window Coding Exercise 7: - maxSubarraySum

---
+++ maxSubarraySum.js
@@ -0,0 +1,27 @@
+///////// Sliding Window Coding Exercise 7: - maxSubarraySum  ///////////
+
+// Given an array of integers and a number, write a function called maxSubarraySum , which finds the maximum
+// sum of a subarray with the length of the number passed to the function.
+// Note that a subarray must consist of consecutive  elements from the original array. In the first example
+// below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) {
+    return null;
+  }
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+console.log(maxSubarraySum([2, -3], 2)); // -1
+console.log(maxSubarraySum([2, 3], 3)); // null
