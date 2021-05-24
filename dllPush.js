7bd3506637874994307d91bb0a36ede935025f23
Hovhannes Kantuni <hkantuni@gmail.com>
2021-05-24 19:02:05 -0400

    DLL Exercise 26: push

---
+++ dllPush.js
@@ -0,0 +1,35 @@
+///  Coding Exercise 26: DLL push ////    Implement the following on the DoublyLinkedList class ///////
+///push////
+// This function should accept a value add a node to the end of the DoublyLinkedList with the given value.
+// It should return the DoublyLinkedList.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+let doublyLinkedList = new DoublyLinkedList();
+console.log(doublyLinkedList.push("5"));
