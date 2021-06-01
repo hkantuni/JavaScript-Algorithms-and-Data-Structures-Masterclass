88504493a2caf555d4ec29ff6e891a7b65819b28
Hovhannes Kantuni <hkantuni@gmail.com>
2021-06-01 14:52:55 -0400

    Searching A Binary Search Tree

---
+++ binarySearchTree.js
@@ -0,0 +1,95 @@
+///////////  Binary Search Trees ///////////
+
+// insert, find and contain methods //////////
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(val) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+
+  ////// same as find but returns true or false ///////
+
+  contains(val) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+let bst = new BinarySearchTree();
+// bst.find(2);
+bst.insert(10);
+bst.insert(5);
+bst.insert(13);
+bst.insert(11);
+bst.insert(2);
+bst.insert(16);
+bst.insert(7);
+bst.insert(7);
+console.log("1", bst.contains(16));
+console.log("2", bst.contains(166));
+console.log("3", bst.find(16));
+console.log(bst);
